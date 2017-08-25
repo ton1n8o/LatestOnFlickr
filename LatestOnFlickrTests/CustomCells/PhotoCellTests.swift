e5c8bebd6f5bb85bad3d372739f851db050fdfca
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-08-25 14:56:12 +0800

    Added Pod, more UI tests

--- LatestOnFlickrTests/CustomCells/PhotoCellTests.swift
+++ LatestOnFlickrTests/CustomCells/PhotoCellTests.swift
@@ -10,9 +10,20 @@ import XCTest
 @testable import LatestOnFlickr
 
 class PhotoCellTests: XCTestCase {
+
+    var sut: PhotoCell!
     
     override func setUp() {
         super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "PhotosViewController") as! PhotosViewController
+        _ = controller.view
+        
+        let tableView = controller.tableView
+        let dataSource = FakeDataSource()
+        tableView?.dataSource = dataSource
+        sut = tableView?.dequeueReusableCell(withIdentifier: "PhotoCell", for: IndexPath(row: 0, section: 0)) as! PhotoCell
     }
     
     override func tearDown() {
@@ -20,8 +31,31 @@ class PhotoCellTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func test_HasUIComponents() {
         
+        XCTAssertNotNil(sut.userNameLabel)
+        XCTAssertNotNil(sut.photoImageView)
+    }
+    
+    func test_ConfigCell_SetsUIElements() {
+        sut.configCell(with: Photo(urlMedium: "", ownerName: "Foo"))
+        
+        XCTAssertEqual(sut.userNameLabel.text, "Foo")
+//        XCTAssertNotNil(sut.photoImageView.image)
     }
     
 }
+
+// 1
+extension PhotoCellTests {
+    class FakeDataSource: NSObject, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'LatestOnFlickr' do
+  use_frameworks!
+
+  pod 'Kingfisher'
+
+  target 'LatestOnFlickrTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end

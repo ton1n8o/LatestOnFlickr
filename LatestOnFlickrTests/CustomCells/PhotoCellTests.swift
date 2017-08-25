b7453244901597d0b0bfec21ce689dc3cb38d65e
Antônio Carlos <antoniocarlos.dev@gmail.com>
2017-08-25 14:13:21 +0800

    PhotosTests

---
+++ LatestOnFlickrTests/CustomCells/PhotoCellTests.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCellTests.swift
+//  LatestOnFlickrTests
+//
+//  Created by Antonio da Silva on 25/08/2017.
+//  Copyright © 2017 TNTStudios. All rights reserved.
+//
+
+import XCTest
+@testable import LatestOnFlickr
+
+class PhotoCellTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+    }
+    
+}
